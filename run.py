e2179491c6a64834bdcbf73634f33fb780f8fae6
sbnn3 <sebastian.neagu@outlook.com>
2022-05-10 09:33:15 +0000

    Declared the variables for snake and food and added the window characters/coordinants for snakes and food

--- run.py
+++ run.py
@@ -8,11 +8,20 @@ curses.noecho()
 window.border(0)
 window.nodelay(1)
 
+# The snake and food variables
+snake = [(4, 10), (4, 9), (4,8)]
+food = (10, 20)
+
 # The snake game structure and logic
 score = 0
 
 while True:
     event = window.getch()
+
+    for c in snake: 
+        window.addch(c[0], c[1], '-')
+
+    window.addch(food[0], food[1], '#')
 
 curses.endwin()
 print(f"The final score is = {score}")
